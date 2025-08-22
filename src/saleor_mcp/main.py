c8cd6e2445840c9c3ae6a1e5a3e184d8f3f97ac5
Marcin Gębala <maarcin.gebala@gmail.com>
2025-08-22 18:54:10 +0200

    Expose app object

--- src/saleor_mcp/main.py
+++ src/saleor_mcp/main.py
@@ -5,6 +5,8 @@ from saleor_mcp.orders import orders_router
 mcp = FastMCP("Saleor MCP Server")
 mcp.mount(orders_router)
 
+app = mcp.http_app
+
 
 def main():
     mcp.run(transport="http", host="0.0.0.0", port=6000)
