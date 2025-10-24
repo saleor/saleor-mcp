e2840b08b59e41ddaa1c8bb2f63622e9b92d0bac
Marcin Gębala <maarcin.gebala@gmail.com>
2025-10-24 12:56:16 +0200

    Add Jinja2 template for docs

--- src/saleor_mcp/tools/utils.py
+++ src/saleor_mcp/tools/utils.py
@@ -7,7 +7,7 @@ utils_router = FastMCP("Utils MCP")
 
 @utils_router.tool()
 def current_domain() -> str:
-    """Return the current domain of the application."""
+    """Return the current domain of the connected Saleor instance."""
 
     headers = get_config_from_headers()
     return headers.api_url
